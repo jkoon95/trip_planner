b4891137d8c57ebcb146099749d54ce7ce5dec6a
haeunsh <95haeunsh@gmail.com>
2024-03-26 10:46:03 +0900

    Initial commit

---
+++ trip_planner_back/src/main/java/kr/or/iei/TripPlannerBackApplication.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlannerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlannerBackApplication.class, args);
+	}
+
+}

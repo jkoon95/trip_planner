cf3ab6ecd52a5b66ae06c3b68340d332cd40c617
theDevKops <gudrb925@naver.com>
2024-03-26 18:32:21 +0900

    2024-03-26 마무리

--- trip_planner_front/src/App.js
+++ trip_planner_front/src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      우왕
-    </div>
-  );
+  return <div className="App">우왕</div>;
 }
 
 export default App;

9350332ab710226663839380e5f85db3bf44a312
ascarlon7777 <159400522+ascarlon7777@users.noreply.github.com>
2024-03-28 06:54:57 +0900

    굿나잇

--- trip_planner_front/src/page/member/Join.js
+++ trip_planner_front/src/page/member/Join.js
@@ -14,91 +14,121 @@ const Join = () => {
   const [memberName, setMemberName] = useState("");
   const [memberNickName, setMemberNickName] = useState("");
   const [memberPhone, setMemberPhone] = useState("");
-  const join = () => {
-    navigate("/");
+  {
+    /*
+  const pwChk = () => {
+    //정규표현식으로 유효성 검사
+    const pwReg = /^(?=.*[a-zA-Z])(?=.*[\W_]).{8,20}$/;
+    if (pwReg.test(memberPw)) {
+      //정규표현식 만족했을때
+      setCheckPwMsg("비밀번호 기준을 만족했습니다.");
+    } else {
+      //정규표현식 만족하지 못했을때
+      setCheckPwMsg(
+        "비밀번호는 영어 대소문자/특수문자 포함으로 8~20글자 입니다."
+      );
+    }
   };
+  //비밀번호 확인을 입력하면, 비밀번호와 일치하는지 체크하는 함수
+  const pwCheck = () => {
+    if (memberPw !== memberPwRe) {
+      setCheckPwMsg("비밀번호가 일치하지 않습니다.");
+    } else {
+      setCheckPwMsg("");
+    }
+  };
+  //회원가입버튼 클릭 시 동작할 이벤트
+
+  const join = () => {
+    if (
+      memberId !== "" &&
+      memberPw !== "" &&
+      memberName !== "" &&
+      memberPhone !== "" &&
+      checkIdMsg === "" &&
+      checkPwMsg === ""
+    ) {
+      const obj = { memberId, memberPw, memberName, memberPhone };
+      axios
+        .post(backServer + "/member/join", obj)
+        .then((res) => {
+          if (res.data.message === "success") {
+            navigate("/login");
+          } else {
+            Swal.fire(
+              "처리중 에러가 발생했습니다. 잠시후 다시 시도해주세요."
+            ).then(() => {
+              navigate("/");
+            });
+          }
+        })
+        .catch((res) => {
+          console.log(res);
+        });
+    } else {
+      Swal.fire("입력값을 확인하세요.");
+    }
+  };
+*/
+  }
   return (
     <section className="contents join">
       <h2>회원가입</h2>
       <div className="join_input_area">
-        <div className="input_wrap">
-          <div className="input_title">
-            <label htmlFor="memberEmail">이메일</label>
-          </div>
-          <div className="input_item">
-            <Input
-              type="text"
-              content="memberEmail"
-              placeholder="abc@google.com"
-              data={memberEmail}
-              setData={setMemberEmail}
-            />
-          </div>
-        </div>
-        <div className="input_wrap">
-          <div className="input_title">
-            <label htmlFor="memberPw">비밀번호</label>
-          </div>
-          <div className="input_item">
-            <Input
-              type="password"
-              content="memberPw"
-              placeholder="8글자~20글자/영어 대소문자 특수문자 포함"
-              data={memberPw}
-              setData={setMemberPw}
-            />
-          </div>
-        </div>
-        <div className="input_wrap">
-          <div className="input_title">
-            <label htmlFor="memberName">이름</label>
-          </div>
-          <div className="input_item">
-            <Input
-              type="text"
-              content="memberName"
-              data={memberName}
-              setData={setMemberName}
-            />
-          </div>
-        </div>
-        <div className="input_wrap">
-          <div className="input_title">
-            <label htmlFor="memberNickName">닉네임</label>
-          </div>
-          <div className="input_item">
-            <Input
-              type="text"
-              content="memberNickName"
-              placeholder="6~10글자"
-              data={memberNickName}
-              setData={setMemberNickName}
-            />
-          </div>
-        </div>
-        <div className="input_wrap">
-          <div className="input_title">
-            <label htmlFor="memberPhone">전화번호</label>
-          </div>
-          <div className="input_item">
-            <Input
-              type="text"
-              content="membePhone"
-              placeholder="010-0000-0000"
-              data={memberPhone}
-              setData={setMemberPhone}
-            />
-          </div>
-        </div>
+        <JoinInputWrap
+          label="이메일"
+          content="memberEmail"
+          type="text"
+          value={memberEmail}
+          onChange={setMemberEmail}
+        />
         <div className="btn_area">
-          <Button
-            text="회원가입"
-            class="btn_primary"
-            clickEvent={join}
-          ></Button>
+          <Button text="회원가입" class="btn_primary"></Button>
         </div>
       </div>
     </section>
+  );
+};
+
+const JoinInputWrap = (props) => {
+  const label = props.label;
+  const type = props.type;
+  const placeholder = props.placeholder;
+  const content = props.content;
+  const data = props.data; //input태그와 연결할 state
+  const setData = props.setData;
+  const chageData = props.chageData;
+  const clickEvent = props.clickEvent;
+  const blurEvent = props.blurEvent;
+  const readonly = props.readonly;
+  const disabled = props.disabled;
+  const inputRef = props.inputRef;
+  const keyupEvent = props.keyupEvent;
+  const checkMsg = props.checkMsg;
+  const changeData = (e) => {
+    setData(e.target.value);
+  };
+  return (
+    <div className="join-input-wrap">
+      <div>
+        <div className="label">
+          <label htmlFor={content}>{label}</label>
+        </div>
+        <Input
+          value={data}
+          onChange={changeData}
+          type={type}
+          id={content}
+          onBlur={blurEvent}
+        />
+      </div>
+      {checkMsg ? <div className="check-msg">{checkMsg}</div> : ""}
+      {/* {checkMsg !== undefined ? (
+        <div className="check-msg">{checkMsg}</div>
+      ) : (
+        ""
+      )} */}
+    </div>
   );
 };
 

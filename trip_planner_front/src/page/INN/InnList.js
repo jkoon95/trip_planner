7d26a8c2c1d24a1ca0b452af5887cc9ee3e6fc68
yoondojoon <blueyerr12@naver.com>
2024-03-27 11:16:30 +0900

    제목 커밋

--- trip_planner_front/src/page/INN/InnList.js
+++ trip_planner_front/src/page/INN/InnList.js
@@ -2,10 +2,13 @@ import ListSideMenu from "./ListSideMenu";
 
 const InnList = () => {
   return (
-    <div className="inn-wrap">
-      <ListSideMenu />
-      <div className="inn-list-wrap">ㅎㅎ</div>
-    </div>
+    <section className="contents">
+      <h2>하하</h2>
+      <div className="inn-wrap">
+        <ListSideMenu />
+        <div className="inn-list-wrap">ㅎㅎ</div>
+      </div>
+    </section>
   );
 };
 

f591e996afd7cb16dcf94fcd641d233767450056
yoondojoon <blueyerr12@naver.com>
2024-03-27 18:21:08 +0900

    css커밋

--- trip_planner_front/src/page/mypage/MyPageMain.js
+++ trip_planner_front/src/page/mypage/MyPageMain.js
@@ -18,8 +18,12 @@ const MypageMain = (props) => {
   const [member, setMember] = useState("");
   const navigate = useNavigate();
 
-  if(!isLogin){
-    Swal.fire({icon: "warning", text: "로그인 후 이용이 가능합니다.", confirmButtonText: "닫기"});
+  if (!isLogin) {
+    Swal.fire({
+      icon: "warning",
+      text: "로그인 후 이용이 가능합니다.",
+      confirmButtonText: "닫기",
+    });
     navigate("/");
   }
   console.log(window.localStorage.getItem("member"));
@@ -35,37 +39,37 @@ const MypageMain = (props) => {
   // },[]);
 
   const [menus, setMenus] = useState([
-    {url: "myBooks", text: "내 예약", active: true},
-    {url: "myTrips", text: "내 여행", active: false},
-    {url: "myCoupons", text: "쿠폰함", active: false},
-    {url: "myLikes", text: "찜 리스트", active: false},
-    {url: "myReviews", text: "내 리뷰 보기", active: false},
-    {url: "myInfo", text: "내 정보 수정", active: false},
+    { url: "myBooks", text: "내 예약", active: true },
+    { url: "myTrips", text: "내 여행", active: false },
+    { url: "myCoupons", text: "쿠폰함", active: false },
+    { url: "myLikes", text: "찜 리스트", active: false },
+    { url: "myReviews", text: "내 리뷰 보기", active: false },
+    { url: "myInfo", text: "내 정보 수정", active: false },
   ]);
-  
-  return(
+
+  return (
     <section className="contents mypage">
       <div className="side_wrap">
         <h2>마이페이지</h2>
         <MypageSideMenu menus={menus} setMenus={setMenus} />
       </div>
       <div className="content_wrap">
-          {/* {member && member.memberType === 1 ? ( */}
-          {isLogin ? (
-            <Routes>
-              <Route path="/myBooks" element={<MyBooks />} />
-              <Route path="/myTrips" element={<MyTrips />} />
-              <Route path="/myCoupons" element={<MyCoupons />} />
-              <Route path="/myLikes" element={<MyLikes />} />
-              <Route path="/myReviews" element={<MyReviews />} />
-              <Route path="/myInfo" element={<MyInfo />} />
-            </Routes>
-            ) : (
-            ""
-          )}
+        {/* {member && member.memberType === 1 ? ( */}
+        {isLogin ? (
+          <Routes>
+            <Route path="/myBooks" element={<MyBooks />} />
+            <Route path="/myTrips" element={<MyTrips />} />
+            <Route path="/myCoupons" element={<MyCoupons />} />
+            <Route path="/myLikes" element={<MyLikes />} />
+            <Route path="/myReviews" element={<MyReviews />} />
+            <Route path="/myInfo" element={<MyInfo />} />
+          </Routes>
+        ) : (
+          ""
+        )}
       </div>
     </section>
   );
-}
+};
 
 export default MypageMain;

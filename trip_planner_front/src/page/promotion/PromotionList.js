99677d6ac308d9219a954a3c78d4192e9e8c7121
ascarlon7777 <159400522+ascarlon7777@users.noreply.github.com>
2024-03-29 14:14:13 +0900

    수정

--- trip_planner_front/src/page/promotion/PromotionList.js
+++ trip_planner_front/src/page/promotion/PromotionList.js
@@ -1,5 +1,24 @@
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import "./promotion.css";
+
 const PromotionList = () => {
-  return <>프로모션 리스트</>;
+  return (
+    <section className="contents">
+      <div className="input_wrap">
+        <div className="input_item">
+          <input id="search" className="input" placeholder="...검색어 입력" />
+          <button className="btn_search" />
+        </div>
+      </div>
+      <ButtonGroup variant="contained" aria-label="Basic button group">
+        <Button>최신순</Button>
+        <Button>가격순</Button>
+        <Button>마감임박</Button>
+      </ButtonGroup>
+    </section>
+  );
 };
 
 export default PromotionList;
+// *snip*

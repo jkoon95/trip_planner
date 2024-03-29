2ae30f2657e6101f390dcfb1229f53b705219fb4
jkoon95 <jkoon106@gmail.com>
2024-03-29 17:40:11 +0900

    투어 등록 작성중

--- trip_planner_back/src/main/java/kr/or/iei/tour/controller/TourController.java
+++ trip_planner_back/src/main/java/kr/or/iei/tour/controller/TourController.java
@@ -1,10 +1,17 @@
 package kr.or.iei.tour.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import kr.or.iei.ResponseDTO;
+import kr.or.iei.tour.model.dto.Tour;
 import kr.or.iei.tour.model.service.TourService;
 
 @CrossOrigin("*")
@@ -14,5 +21,10 @@ public class TourController {
 	@Autowired
 	private TourService tourService;
 	
-	
+//	@PostMapping("/reg")
+//	public ResponseEntity<ResponseDTO> insertTour(@ModelAttribute Tour tour, @ModelAttribute MultipartFile thumbnail, @RequestAttribute int partnerNo) {
+//		tour.setPartnerNo(partnerNo);
+//		String savepath = root+"/tour/";
+//		
+//	}
 }

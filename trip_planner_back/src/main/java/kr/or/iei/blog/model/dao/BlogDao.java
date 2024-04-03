21f1fad15ecac43a1b72fe0beb4d41755ce94f06
theDevKops <gudrb925@naver.com>
2024-04-03 09:52:37 +0900

    24.04.03 첫 커밋

--- trip_planner_back/src/main/java/kr/or/iei/blog/model/dao/BlogDao.java
+++ trip_planner_back/src/main/java/kr/or/iei/blog/model/dao/BlogDao.java
@@ -2,13 +2,8 @@ package kr.or.iei.blog.model.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import kr.or.iei.blog.model.dto.Blog;
 
 @Mapper
 public interface BlogDao {
-
-	int insertBlog(Blog blog);
-
-	int insertBlogDate(Blog blog);
 
 }

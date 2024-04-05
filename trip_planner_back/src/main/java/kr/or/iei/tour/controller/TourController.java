f81a47ed37c86bbf8e6cfe2a5fa53e26414ce4a1
jkoon95 <jkoon106@gmail.com>
2024-04-05 16:10:25 +0900

    투어 상품리스트 중간완

--- trip_planner_back/src/main/java/kr/or/iei/tour/controller/TourController.java
+++ trip_planner_back/src/main/java/kr/or/iei/tour/controller/TourController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 

2a8899b1c15d552ca6aa0e954fc29ecb617c583a
ascarlon7777 <159400522+ascarlon7777@users.noreply.github.com>
2024-04-05 11:12:15 +0900

    로그아웃 수정

--- trip_planner_front/src/page/notice/NoticeList.js
+++ trip_planner_front/src/page/notice/NoticeList.js
@@ -1,4 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const NoticeList = () => {
+  const backServer = process.env.REACT_APP_BACK_SERVER;
+
+  const [noticeList, setNoticeList] = useState([]);
+  const [pageInfo, setPageInfo] = useState([]);
+  const [reqPage, setReqPage] = useState(1);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(backServer + "/notice/list/" + reqPage)
+      .then((res) => {
+        console.log(res.data);
+        setNoticeList(res.data.data.noticeList);
+        setPageInfo(res.data.data.pi);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  }, [reqPage]);
   return (
     <section className="contents">
       <h2>공지사항</h2>
